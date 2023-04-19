33ddc0dfcb6c4e641c4fc067e667df565482c3e9
juzjus10 <39343743+juzjus10@users.noreply.github.com>
2023-04-19 11:33:56 +0800

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,10 +20,94 @@ export default function App(props: AppProps) {
         withGlobalStyles
         withNormalizeCSS
         theme={{
-          /** Put your mantine theme override here */
           colorScheme: "dark",
+          colors: {
+            primary: [
+              "#e3bfb5",
+              "#dcafa3",
+              "#d59f90",
+              "#cc8b79",
+              "#b95f46",
+              "#a6563f",
+              "#944c38",
+              "#814331",
+              "#6f392a",
+              "#4a261c"
+            ],
+            gradientFrom: [
+              "#d7bfd9",
+              "#cdafcf",
+              "#c3a0c5",
+              "#b990bc",
+              "#9b609f",
+              "#8b568f",
+              "#7c4d7f",
+              "#714674",
+              "#5d3a5f",
+              "#3e2640"
+            ],
+            gradientTo: [
+              "#c7b1e7",
+              "#b99ee1",
+              "#ab8adb",
+              "#9d77d5",
+              "#733cc3",
+              "#6836af",
+              "#5c309c",
+              "#512a88",
+              "#432371",
+              "#2e184e"
+            ]
+          },
+          primaryShade: 3,
+          primaryColor: "primary",
+          defaultRadius: "sm",
+          defaultGradient: {
+            deg: 90,
+            to: "gradientTo",
+            from: "gradientFrom"
+          },
+          components: {
+            Button: {
+              styles: {
+                root: {
+                  borderWidth: 1
+                }
+              }
+            },
+            Chip: {
+              styles: {
+                label: {
+                  borderWidth: 1
+                }
+              }
+            },
+            Input: {
+              styles: {
+                input: {
+                  borderWidth: 1
+                }
+              }
+            },
+            Pagination: {
+              styles: {
+                item: {
+                  borderWidth: 1
+                }
+              }
+            },
+            Switch: {
+              styles: {
+                track: {
+                  borderWidth: 1
+                }
+              }
+            }
+          },
+     
         }}
       >
+         
         <Component {...pageProps} />
       </MantineProvider>
     </>
