41c4382cd801f9d2f003b5dd861ef729d102d33a
Juzz <juzjus10@gmail.com>
2023-04-26 00:10:41 +0800

    fixed db relationship

---
+++ prisma/migrations/20230425160756_fixed_relationship/migration.sql
@@ -0,0 +1,102 @@
+-- CreateTable
+CREATE TABLE `Appointment` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `status` VARCHAR(45) NOT NULL,
+    `appointment_time` TIME(0) NOT NULL,
+    `date_of_appointment` DATE NOT NULL,
+    `created_at` DATE NOT NULL,
+    `patient_id` INTEGER NOT NULL,
+    `doctor_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Doctor` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `doctor_name` VARCHAR(45) NOT NULL,
+    `gender` VARCHAR(45) NOT NULL,
+    `dob` DATE NOT NULL,
+    `hire_date` DATE NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Patient` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `address` LONGTEXT NOT NULL,
+    `age` INTEGER NOT NULL,
+    `sex` VARCHAR(20) NOT NULL,
+    `civil_status` VARCHAR(20) NOT NULL,
+    `dob` DATE NOT NULL,
+    `mobile_no` INTEGER NOT NULL,
+    `emergency_contact` VARCHAR(20) NOT NULL,
+    `emergency_mobile_no` INTEGER NOT NULL,
+    `medical_history` LONGTEXT NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Records` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `ammount_paid` FLOAT NOT NULL,
+    `balance` FLOAT NOT NULL,
+    `commission` FLOAT NOT NULL,
+    `patient_id` INTEGER NOT NULL,
+    `doctor_id` INTEGER NOT NULL,
+
+    UNIQUE INDEX `Records_patient_id_key`(`patient_id`),
+    UNIQUE INDEX `Records_doctor_id_key`(`doctor_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Treatment` (
+    `id` INTEGER NOT NULL,
+    `treatment` VARCHAR(50) NOT NULL,
+    `cost` INTEGER NOT NULL,
+    `appointment_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL,
+    `username` TEXT NOT NULL,
+    `password` TEXT NOT NULL,
+    `firstname` TEXT NOT NULL,
+    `lastname` TEXT NOT NULL,
+    `middlename` TEXT NOT NULL,
+    `user_level` TEXT NOT NULL,
+    `email` TEXT NOT NULL,
+    `patient_id` INTEGER NULL,
+    `doctor_id` INTEGER NULL,
+
+    UNIQUE INDEX `User_patient_id_key`(`patient_id`),
+    UNIQUE INDEX `User_doctor_id_key`(`doctor_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Appointment` ADD CONSTRAINT `Appointment_patient_id_fkey` FOREIGN KEY (`patient_id`) REFERENCES `Patient`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Appointment` ADD CONSTRAINT `Appointment_doctor_id_fkey` FOREIGN KEY (`doctor_id`) REFERENCES `Doctor`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Records` ADD CONSTRAINT `Records_patient_id_fkey` FOREIGN KEY (`patient_id`) REFERENCES `Patient`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Records` ADD CONSTRAINT `Records_doctor_id_fkey` FOREIGN KEY (`doctor_id`) REFERENCES `Doctor`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Treatment` ADD CONSTRAINT `Treatment_appointment_id_fkey` FOREIGN KEY (`appointment_id`) REFERENCES `Appointment`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `User` ADD CONSTRAINT `User_patient_id_fkey` FOREIGN KEY (`patient_id`) REFERENCES `Patient`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `User` ADD CONSTRAINT `User_doctor_id_fkey` FOREIGN KEY (`doctor_id`) REFERENCES `Doctor`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
